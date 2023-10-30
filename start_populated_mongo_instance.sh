3aded758ae07095dfeaac7f0dee0533a183f1063
JP <38257122+uxjp@users.noreply.github.com>
2023-10-30 00:13:04 +0000

    scripts

--- start_populated_mongo_instance.sh
+++ start_populated_mongo_instance.sh
@@ -1,6 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 IMAGE_NAME=${1:-mongo}
+CONTAINER_NAME=${2:-mongodb_container}
 
 docker build -t $IMAGE_NAME .
 
+docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+
+sleep 5 #necessary
+docker exec -it mongodb_container mongosh
