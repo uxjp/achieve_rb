49dd3b1e2731b593af79e5cd75fa6904113c3a48
JP <38257122+uxjp@users.noreply.github.com>
2023-11-04 13:13:50 +0000

    handle a POST

--- crud_server/server.rb
+++ crud_server/server.rb
@@ -6,5 +6,14 @@ server.mount_proc '/' do |req, res|
   res.body = "Hello, Ruby HTTP server\n"
 end
 
+server.mount_proc '/product' do |req, res|
+  if req.request_method == 'POST'
+    res.body = "product just hit\n"
+  else
+    res.status = 405
+    res.body = "Method not allowed\n"
+  end
+end
+
 trap('INT') { server.shutdown }
 server.start
