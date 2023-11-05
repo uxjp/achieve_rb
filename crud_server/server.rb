92973631646244e0960525f2e19348f47c36ad86
JP <38257122+uxjp@users.noreply.github.com>
2023-11-05 22:11:59 +0000

    access database in GET route

--- crud_server/server.rb
+++ crud_server/server.rb
@@ -1,4 +1,5 @@
 require 'webrick'
+require 'mongo'
 
 server = WEBrick::HTTPServer.new(Port:8000)
 
@@ -13,16 +14,27 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
   def initialize(server, dog)
     super(server)
     @dog = dog
+    @client = Mongo::Client.new(['localhost:27017'], database: 'mysupermarket')
   end
+
 
   def do_GET(request, response)
     params = request.query
     database = params['database']
-    collection = params['collection']
+    collection_name = params['collection']
 
-    puts @dog
+    collection = @client[collection_name]
+
+    results = collection.find()
+
+    puts "\n\n"
+
+    results.each do |doc|
+      puts doc
+    end
+
     response.status = 200
-    response.body = "collection #{database} #{collection}"
+    response.body = "collection #{database} #{collection_name}"
   end
 end
 
