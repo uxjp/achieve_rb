bce21bb0e231fbd82d8b736bf9434f194ccebbce
JP <38257122+uxjp@users.noreply.github.com>
2023-11-05 21:53:57 +0000

    intance variable in ruby

--- crud_server/server.rb
+++ crud_server/server.rb
@@ -4,25 +4,29 @@ server = WEBrick::HTTPServer.new(Port:8000)
 
 dog = 'dog au au'
 
-puts dog
 
 server.mount_proc '/' do |req, res|
   res.body = "Hello, Ruby HTTP server\n"
 end
 
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, dog)
+    super(server)
+    @dog = dog
+  end
+
   def do_GET(request, response)
     params = request.query
     database = params['database']
     collection = params['collection']
 
-    puts dog
+    puts @dog
     response.status = 200
     response.body = "collection #{database} #{collection}"
   end
 end
 
-server.mount('/products', MyServlet)
+server.mount('/products', MyServlet, dog)
 
 trap('INT') { server.shutdown }
 server.start
